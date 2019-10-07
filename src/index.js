2c6f7a17816f304e6f4b050f048805ef8d151d3d
andreisoroka <unickpro@gmail.com>
2019-10-07 04:40:35 +0300

    add store

--- src/index.js
+++ src/index.js
@@ -1,20 +1,37 @@
 import './styles/style.css';
 import Graph from './modules/Graph.js';
-// import Store from './modules/Store.js';
+import Store from './modules/Store.js';
 
-async function init() {
-    const temperature = await import("./serverMocData/temperature.json");
-    console.log(temperature)
-}
-init().then();
-
-const dataY = [
-  25, 20, 30, 20, 15, 25, 20, 20, 25, 26, 20, 27, 30, 41, 34, 37, 30, 40, 31, 29, 27, 25,
-];
+// const dataY = [
+//   25, 20, 30, 20, 15, 25, 20, 20, 25, 26, 20, 27, 30, 41, 34, 37, 30, 40, 31, 29, 27, 25,
+// ];
 
 const CANVAS_WIDTH = 500;
 const CANVAS_HEIGHT = 300;
 
 
-const graph = new Graph('graph', CANVAS_WIDTH, CANVAS_HEIGHT);
-graph.draw(dataY);
+class App {
+  constructor(store, graph) {
+    this.store = store;
+    this.graph = graph;
+  }
+
+  drawGraph(start = 1881, end = 2006, type = 'temperature') {
+    let data;
+    if (type === 'temperature') {
+      data = this.store.getTemperatureData(start, end);
+      console.log(data);
+    }
+    // this.graph.draw(dataY);
+  }
+
+
+}
+
+
+async function init() {
+  const app = new App(new Store, new Graph('graph', CANVAS_WIDTH, CANVAS_HEIGHT));
+  app.drawGraph();
+}
+
+init().then();
--- src/modules/Store.js
+++ src/modules/Store.js
@@ -1,3 +1,148 @@
-export default class Store {
+const NAME_TEMPERATURE = 'temperature';
+const NAME_PRECIPITATION = 'precipitation';
 
+export default class Store {
+  constructor() {
+    this.db = this._connectDB(window.indexedDB);
+  }
+
+  /**
+   * Connect to indexedDB
+   * @param idb
+   * @returns {Promise}
+   * @private
+   */
+  _connectDB(idb) {
+    if (!idb) {
+      return Promise.reject('Not supported indexedDB');
+    }
+
+    return new Promise((resolve, reject) => {
+      const request = idb.open('weather', 1);
+
+      request.onerror = reject;
+      request.onsuccess = () => {
+        const db = request.result;
+        db.onversionchange = function () {
+          db.close();
+          alert('DB is deprecated');
+          location.reload();
+        };
+        return resolve(db);
+      };
+      request.onupgradeneeded = (e) => {
+        e.currentTarget.result.createObjectStore(NAME_TEMPERATURE);
+        e.currentTarget.result.createObjectStore(NAME_PRECIPITATION);
+        return this._connectDB();
+      };
+    });
+  }
+
+  /**
+   *
+   * @param start
+   * @param end
+   * @returns {Promise<void>}
+   */
+  async getTemperatureData(start, end) {
+    // first load
+    if (true) {
+      await this._loadTemperatureDataFromServer();
+    }
+  }
+
+  /**
+   * Load temperature data from server
+   * @returns {Promise<void>}
+   * @private
+   */
+  async _loadTemperatureDataFromServer() {
+    const data = await Store.loadingDataFromServer(NAME_TEMPERATURE);
+    await this._transactionFromHandle(NAME_TEMPERATURE, objectStore => {
+      let cursor = 0;
+
+      let cursorStartByYear = 0;
+      let currentYear = 0;
+      let commonDataByYears = 0;
+
+      let months = {};
+      let cursorStartByMonth = 0;
+      let currentMonth = 0;
+      let commonDataByMonth = 0;
+
+      while (data[cursor]) {
+        const item = data[cursor];
+        const [year, month] = item.t.split('-').map(i => +i);
+
+        if (currentMonth !== month) {
+          if (currentMonth) {
+            saveDataByMonth();
+          }
+
+          commonDataByYears += commonDataByMonth;
+          if (currentYear !== year) {
+            if (currentYear) {
+              saveDataByYear();
+              months = {};
+            }
+
+            commonDataByYears = 0;
+            currentYear = year;
+            cursorStartByYear = cursor;
+          }
+
+          cursorStartByMonth = cursor;
+          commonDataByMonth = item.v;
+          currentMonth = month;
+        } else {
+          commonDataByMonth += item['v'] * 10;
+        }
+        ++cursor;
+      }
+      saveDataByMonth();
+      saveDataByYear();
+
+      function saveDataByYear() {
+        const v = Math.round(commonDataByYears / (cursor - cursorStartByYear) * 10) / 100;
+        objectStore.put({ v, months }, currentYear);
+      }
+
+      function saveDataByMonth() {
+        months[currentMonth] = Math.round(commonDataByMonth / (cursor - cursorStartByMonth) * 10) / 100;
+      }
+    });
+  }
+
+  /**
+   * Handler for multiple transaction
+   * @param {String} name - table name
+   * @param {Function} fnHandle - handler for transaction
+   * @returns {Promise<*>}
+   * @private
+   */
+  async _transactionFromHandle(name, fnHandle) {
+    const db = await this.db;
+    const transaction = db.transaction(name, 'readwrite');
+
+    const objectStore = transaction.objectStore(name);
+    fnHandle(objectStore);
+    return new Promise(resolve => {
+      transaction.oncomplete = () => {
+        resolve();
+      };
+    });
+  };
+
+  /**
+   * Mock data from server (imitation query)
+   * @param type
+   * @returns {Promise<Array>}
+   */
+  static async loadingDataFromServer(type) {
+    if (type === NAME_TEMPERATURE) {
+      return import('../serverMocData/temperature.json');
+    }
+
+    return [];
+  }
 }
